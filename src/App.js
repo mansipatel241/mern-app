8436e1a3d96f65d1da8bb0ca041eee504fe38f80
Mansi Patel <52688826+mansipatel241@users.noreply.github.com>
2022-04-26 15:26:22 +0530

    React Route Configured

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import CreateTodo from './components/create-todo.component';
+import EditTodo from './components/edit-todo.component';
+import TodosList from './components/todos-list.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+      <div className="container">
+        <h2>MERN-Stack Todo App</h2>
+    <Routes>
+    
+        <Route path="/" exact element={<TodosList/>} />
+        <Route path="/edit/:id"  element={<EditTodo/>} />
+        <Route path="/create"  element={<CreateTodo/>}/>
+      
+    </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/todos-list.component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TodosList() {
+    return (
+        <div>
+            <p>Welcome to Todo List Component!!</p>
+        </div>
+    );
+}
+
+export default TodosList;
